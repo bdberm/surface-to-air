547bbf88173423764abc182dc63fcf49a9216400
Benjamin Berman <bdberm@gmail.com>
2017-11-10 17:23:21 -0500

    Add explosion iamge

--- bundle.js
+++ bundle.js
@@ -165,6 +165,7 @@ document.addEventListener("DOMContentLoaded",() => {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__bomb__ = __webpack_require__(6);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__util__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__collisions__ = __webpack_require__(7);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__explosion__ = __webpack_require__(8);
 
 
 
@@ -187,11 +188,12 @@ class Board {
     this.mainCannon = new __WEBPACK_IMPORTED_MODULE_1__cannon__["a" /* default */]([canvas.width/2, canvas.height]);
     this.lasers = [];
     this.bombs = [];
+    this.lasers = [];
+    this.explosion = new __WEBPACK_IMPORTED_MODULE_6__explosion__["a" /* default */]([100,100]);
     this.render = this.render.bind(this);
     this.generateBombs();
     this.checkCollisions();
     this.explode = new Audio('./assets/explosion.wav');
-
   }
 
   generateBombs() {
@@ -217,7 +219,7 @@ class Board {
   }
 
   render() {
-
+    this.explosion.render(this.ctx);
     this.ctx.clearRect(0,0, this.width, this.height);
     this.crossHair.render(this.ctx);
     this.renderCannon();
@@ -485,6 +487,48 @@ const checkPosInHitbox = (pos, hitbox) => {
 
   return inBox;
 };
+
+
+/***/ }),
+/* 8 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+const EXPLOSION_WIDTH = 50;
+const EXPLOSION_HEIGHT = 50;
+
+class Explosion {
+  constructor(pos) {
+    this.pos = pos;
+    this.width = EXPLOSION_WIDTH;
+    this.height = EXPLOSION_HEIGHT;
+    this.img = new Image();
+    this.img.src = "./assets/explosion_sprite.png";
+    this.frame = 0;
+    this.animate();
+  }
+
+  render(ctx) {
+
+    const row = Math.floor(this.frame/4);
+    const col = this.frame % 4;
+    const xSpriteOffset = col * 128;
+    const ySpriteOffset = row * 128;
+    ctx.drawImage(this.img, xSpriteOffset, ySpriteOffset, 128, 128, this.pos[0], this.pos[1], this.width, this.height);
+  }
+
+  animate() {
+    let explode = window.setInterval(() => {
+      this.frame += 1;
+    },50);
+
+    if (this.frame > 35) {
+      window.clearInterval(explode);
+    }
+  }
+}
+
+/* harmony default export */ __webpack_exports__["a"] = (Explosion);
 
 
 /***/ })
